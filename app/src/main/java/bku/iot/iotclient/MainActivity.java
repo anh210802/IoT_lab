63e974a2bc2189226a9cb84fd6a002bad5ddcceb
anh210802 <anhxanh2002@gmail.com>
2024-04-27 18:47:49 +0700

    init code

---
+++ app/src/main/java/bku/iot/iotclient/MainActivity.java
@@ -0,0 +1,106 @@
+package bku.iot.iotclient;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.github.angads25.toggle.interfaces.OnToggledListener;
+import com.github.angads25.toggle.model.ToggleableView;
+import com.github.angads25.toggle.widget.DayNightSwitch;
+import com.github.angads25.toggle.widget.LabeledSwitch;
+
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+import java.nio.charset.StandardCharsets;
+
+public class MainActivity extends AppCompatActivity {
+    MQTTHelper mqttHelper;
+    TextView txtTemp, txtHumi;
+    DayNightSwitch toggle_light;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        txtTemp = findViewById(R.id.txtTemperature);
+        txtHumi = findViewById(R.id.txtHumidity);
+        toggle_light = findViewById(R.id.toggle_light);
+        toggle_light.setOnToggledListener(new OnToggledListener() {
+            @Override
+            public void onSwitched(ToggleableView toggleableView, boolean isOn) {
+                if (isOn) {
+                    sendDataMQTT("dashboard_iot/feeds/V3", "1");
+                } else {
+                    sendDataMQTT("dashboard_iot/feeds/V3", "0");
+                }
+            }
+        });
+
+        startMQTT();
+    }
+
+    public void sendDataMQTT(String topic, String value) {
+        MqttMessage msg = new MqttMessage();
+        msg.setId(1234);
+        msg.setQos(0);
+        msg.setRetained(false);
+
+        byte[] b = value.getBytes(StandardCharsets.UTF_8);
+        msg.setPayload(b);
+
+        try {
+            mqttHelper.mqttAndroidClient.publish(topic, msg);
+        } catch (MqttException ignored){
+        }
+    }
+    public void startMQTT() {
+        mqttHelper = new MQTTHelper(this);
+        mqttHelper.setCallback(new MqttCallbackExtended() {
+            @Override
+            public void connectComplete(boolean reconnect, String serverURI) {
+
+            }
+
+            @Override
+            public void connectionLost(Throwable cause) {
+
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage message) throws Exception {
+                Log.d("TEST", topic + ":::" + message.toString());
+                if(topic.contains("V1")) {
+                    txtTemp.setText(message.toString() + "°C");
+                } else if (topic.contains("V2")) {
+                    txtHumi.setText(message.toString() + "%");
+                } else if (topic.contains("V3")) {
+                    if (message.toString().equals("1")) {
+                        toggle_light.setOn(true);
+                    } else {
+                        toggle_light.setOn(false);
+                    }
+                }
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken token) {
+
+            }
+        });
+    }
+}
